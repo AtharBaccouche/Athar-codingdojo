0e0b20c87a5c8b9d0b225559ebc8b912cc455488
AtharBaccouche <baccouche.athar@gmail.com>
2022-10-25 21:55:14 +0200

    final

--- Python-Flask/Week1/Day2/core-assignment/Banck_acc.py
+++ Python-Flask/Week1/Day2/core-assignment/Banck_acc.py
@@ -32,3 +32,31 @@ class BankAccount:
     def print_all_accounts(cls):
         for account in cls.accounts:
             account.display_account_info()
+
+class User:
+
+    def __init__(self, name):
+        self.name = name
+        self.account = {
+            "account1" : BankAccount(.02,1000),
+            "account2" : BankAccount(.05,3000)
+        }
+        
+
+    def display_user_balance(self):
+        print(f"User: {self.name}, account1 Balance: {self.account['account1'].display_account_info()}")
+        print(f"User: {self.name}, account2 Balance: {self.account['account2'].display_account_info()}")
+        return self
+
+    def transfer_money(self,amount,user):
+        self.amount -= amount
+        user.amount += amount
+        self.display_user_balance()
+        user.display_user_balance()
+        return self
+
+
+adrien = User("Adrien")
+
+adrien.account['account1'].deposit(100)
+adrien.display_user_balance()
