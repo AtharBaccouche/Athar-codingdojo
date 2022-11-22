2425b65f26d1cdafe2a15554b81d783c4e0988a8
AtharBaccouche <baccouche.athar@gmail.com>
2022-11-22 11:31:55 +0100

    conver

--- MERN/Week1/Day1/core-assignment/predict/prdict.js
+++ MERN/Week1/Day1/core-assignment/predict/prdict.js
@@ -32,9 +32,9 @@ console.log(hashedPassword);//undefined
 
 //Problem4
 const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
-const [,first] = numbers;
-const [,,,second] = numbers;
-const [,,,,,,,,third] = numbers;
+const [,first] = numbers;//2
+const [,,,second] = numbers;//5
+const [,,,,,,,,third] = numbers;//2
 //Predict the output
 console.log(first == second);//False
 console.log(first == third);//True
