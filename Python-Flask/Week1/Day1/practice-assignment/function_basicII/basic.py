b28926b2dcc97842e1bb80b8b51a9ee28e0ad882
AtharBaccouche <baccouche.athar@gmail.com>
2023-01-30 14:53:31 +0100

    finalll

--- Python-Flask/Week1/Day1/practice-assignment/function_basicII/basic.py
+++ Python-Flask/Week1/Day1/practice-assignment/function_basicII/basic.py
@@ -12,6 +12,31 @@ def print_and_return(list):
 print( print_and_return([65,25]))
 
 def first_plus_length(list):
-    sum=list[0]+len(list)
-sum=first_plus_length([15,2,3,8])
+    return list[0]+len(list)
+    
+print(first_plus_length([15,2,3,8]))
+
+
+
+
+def values_grater_then_second(list):
+    if len(list)<2:
+        return False
+    output=[]
+    for i in range (0,len(list)):
+        if list[i]>list[1]:
+            output.append(list[i])
+            print(len(output))
+    return(output)
+print(values_grater_then_second([15]))
+
+def this_length_values(size, value):
+    output=[]
+    for i in range (0,size):
+        output.append(value)
+    return(output)
+print(this_length_values(3,8))
+
+
+
 
